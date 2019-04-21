ee1ef4ef20f48c1bd71f57230e85499b050c3fc2
Ashok Gudivada <ashok.gudivada@eagles.oc.edu>
2019-04-21 10:05:59 -0400

    first commmit to github remote

---
+++ react/src/components/about/AboutPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutPage = () => (
+    <div>
+        <h2>About</h2>
+        <p>
+            This app uses
+            <ul>
+                <li>React</li>
+                <li>Redux</li>
+                <li>React Router</li>
+                <li>And many ....</li>
+            </ul>
+        </p>
+    </div>
+  );
+
+export default AboutPage;
